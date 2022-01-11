022aaa756c60b8d69e0be37b5f33c84967a9c887
Jabair Khan <77592540+jabair14@users.noreply.github.com>
2022-01-10 19:34:12 -0500

    Delete dbConfig.js

--- config/dbConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: 'corpbonds-1.cksav5saijkc.us-east-1.rds.amazonaws.com',
-    USER: 'postgres_bonds',
-    PASSWORD: 'corpbonds',
-    DB: 'corpbonds_db',
-    dialect: 'postgres',
-}
