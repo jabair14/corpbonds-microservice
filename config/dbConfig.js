309c8323b90477b30e1c8667f06fa39d50d5c56b
Jabair <jabairk@gmail.com>
2022-01-18 16:56:04 -0500

    ..

---
+++ config/dbConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    HOST: 'ec2-52-70-120-204.compute-1.amazonaws.com',
+    USER: 'vcegorbpzfmnlg',
+    PASSWORD: 'c930c8113fe628374b756d96c08cc4406ccac11d6d4c41f5750a25dff8850c2f',
+    DB: 'dfrb5ooukq05dm',
+    dialect: 'postgres',
+    // define: {
+    //     timestamps: false
+    // }
+}
