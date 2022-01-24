a51408b22f4e092602f049232355c4b00f9fd1bb
Jabair <jabairk@gmail.com>
2022-01-24 11:59:30 -0500

    latest

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 4000
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 
 //routers
 const routers = require('./routes/bondRouter.js')
